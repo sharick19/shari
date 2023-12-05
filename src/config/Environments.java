62f36de15679fe26743361943b7ce7080f3785e1
sharick19 <karly.blue14@gmail.com>
2023-12-05 17:42:52 -0400

    project

--- src/config/Environments.java
+++ src/config/Environments.java
@@ -2,7 +2,7 @@ package config;
 
 public class Environments {
     public static String lag = "es";
-    public static String pathFile = "C:/Users/Adm/Desktop/U.A.T.F/2DO_SEMESTRE/PROGRAMACION_II/EXAMEN/2DOPARCIAL/data/user.txt";
-     public static String pathFile_two = "C:/Users/Adm/Desktop/U.A.T.F/2DO_SEMESTRE/PROGRAMACION_II/EXAMEN/2DOPARCIAL/data/product.txt";
+    public static String pathFile = "C:/Users/Adm/Desktop/U.A.T.F/2DO_SEMESTRE/PROGRAMACION_II/EXAMEN/2DOPARCIAL.com/data/user.txt";
+     public static String pathFile_two = "C:/Users/Adm/Desktop/U.A.T.F/2DO_SEMESTRE/PROGRAMACION_II/EXAMEN/2DOPARCIAL.com/data/product.txt";
 }
 
